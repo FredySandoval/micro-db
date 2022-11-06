21b06b5a406f17c2f51ef923d2553e9976546678
Fredy Sandoval <45242501+FredySandoval@users.noreply.github.com>
2022-11-06 02:00:47 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+
 if ! command -v unzip &> /dev/null
 then
+    echo "installing unzip..."
     sudo apt-get install unzip
 fi
+
 if ! command -v fnm &> /dev/null
 then
+    echo "installing fnm"
     curl -fsSL https://fnm.vercel.app/install | bash
     export PATH=/home/$USER/.fnm:$PATH
     eval "`fnm env`"
 fi
+
 if ! command -v node &> /dev/null
 then
+    echo "installing node"
     fnm install v16
 fi
