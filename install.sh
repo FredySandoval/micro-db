aefc6f3fbfe5dc6c48941572a6473f34c6d43577
Fredy Sandoval <45242501+FredySandoval@users.noreply.github.com>
2022-11-06 02:10:15 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-if ! command -v unzip &> /dev/null
-then
-    echo "installing unzip..."
-    sudo apt-get install unzip
-fi
-echo "fredy sandoval 1"
-command -v node
-if ! command -v node &> /dev/null
-then
-    echo "installing fnm"
-    curl -fsSL https://fnm.vercel.app/install | bash
-    export PATH=/home/$USER/.fnm:$PATH
-    eval "`fnm env`"
-fi
+# if ! command -v unzip &> /dev/null
+# then
+#     echo "installing unzip..."
+#     sudo apt-get install unzip
+# fi
+# echo "fredy sandoval 1"
+# command -v node
+# if ! command -v node &> /dev/null
+# then
+#     echo "installing fnm"
+#     curl -fsSL https://fnm.vercel.app/install | bash
+#     export PATH=/home/$USER/.fnm:$PATH
+#     eval "`fnm env`"
+# fi
 
-if ! command -v node &> /dev/null
-then
-    echo "installing node"
-    fnm install v16
+# if ! command -v node &> /dev/null
+# then
+#     echo "installing node"
+#     fnm install v16
+# fi
+if ! [ -x "$(command -v node )" ]; then
+    echo "Dependency node is not installed, Install it and run this script again"
 fi
