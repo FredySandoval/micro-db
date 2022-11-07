252b53b58bec3015f4e8baffe1022f794ddfdbfd
Fredy Sandoval <45242501+FredySandoval@users.noreply.github.com>
2022-11-07 17:15:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ if ! command -v unzip &> /dev/null
 then
     echo "installing unzip..."
     sudo apt-get install unzip
+else
+    echo "unzip already installed..."
 fi
 
 
@@ -14,18 +16,22 @@ if ! command -v node &> /dev/null
 then
     echo "installing fnm..."
     curl -fsSL https://fnm.vercel.app/install | bash
-    export PATH=/home/$USER/.fnm:$PATH
-    eval "`fnm env`"
+else
+    echo "fnm already installed..."
 fi
 
 if ! command -v node &> /dev/null
 then
     echo "installing node..."
     fnm install v16
+else
+    echo "node already install..."
 fi
 
 if ! command -v pm2 &> /dev/null
 then
     echo "installing pm2..."
     npm install pm2@latest -g
+else
+    echo "pm2 already installed..."
 fi
